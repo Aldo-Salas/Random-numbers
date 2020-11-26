eb5c35fdedf1c5cf8224d4baf82c871a15ad4faf
Aldo Salas <salas_rdz@outlook.es>
2020-11-25 22:55:51 -0600

    Colocando comillas simples en script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const randomNumberBtn = document.querySelector('.container-button');
 
 randomNumberBtn.addEventListener('click', () => {
 	// Create random number
-	const randomNumberEl = document.getElementById("random-number")
+	const randomNumberEl = document.getElementById('random-number')
 	const min = 0
 	const max = 101
 	const randomNumber = Math.floor(Math.random() * (max - min)) + min
