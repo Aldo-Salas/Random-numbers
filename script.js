dcab1f1b4bd68d8e69ca265cbdf547c614641e2d
Aldo-Salas <salas_rdz@outlook.es>
2021-05-26 11:43:22 -0500

    Mejorando estilos y cambios en archivo script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const randomNumberBtn = document.querySelector('.container-button');
+const randomNumberBtn = document.querySelector(".container-button");
 
-randomNumberBtn.addEventListener('click', () => {
-	// Create random number
-	const randomNumberEl = document.getElementById('random-number')
-	const min = 0
-	const max = 101
-	const randomNumber = Math.floor(Math.random() * (max - min)) + min
-	randomNumberEl.innerHTML = `${randomNumber}`
+randomNumberBtn.addEventListener("click", () => {
+  // Create random number
+  const randomNumberEl = document.getElementById("random-number");
+  const min = 0;
+  const max = 101;
+  const randomNumber = Math.floor(Math.random() * (max - min)) + min;
+  randomNumberEl.textContent = `${randomNumber}`;
 
-	// Copy to clipboard
-	const randomNumberText = randomNumberEl.textContent;
-  	copyToClipboard(randomNumberText)
-})
+  // Copy to clipboard
+  const randomNumberText = randomNumberEl.textContent;
+  copyToClipboard(randomNumberText);
+});
 
 function copyToClipboard(text) {
-  	navigator.clipboard.writeText(text)
+  navigator.clipboard.writeText(text);
 }
